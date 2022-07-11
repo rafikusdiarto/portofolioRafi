88d093d4ff7fe927c05148893f81b6159669a773
unknown <rafikusdiarto1707@gmail.com>
2022-07-11 11:10:15 +0700

    progress2.1

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,39 +7,43 @@ export const about = () => {
     return (
     <>
     {/* about */}
-      <div class="container">
-        <div class="row">
-          <div class="row d-flex justify-content-between my-5">
-            <img src="../assets/img/me.jpg" class="image col-xl-5 d-flex justify-content-center roundedCircle" alt="" />
-            <div class="col-6 mt-5 ">
-              <h2 class="my-2">Hello I'm</h2>
-              <h1><span>Muhammad Rafi Kusdiarto</span></h1>
-              <h5 class="mt-2">Frontend Web Developer</h5>
-              <div class="icon my-3">
-                <a href="https://github.com/rafikusdiarto?tab=repositories" class="btn btn-social-icon btn-github">
-                  <span class="fa fa-github"></span>
-                </a>
-                <a href="https://www.instagram.com/muhmmad.rafii/" class="btn btn-social-icon btn-instagram mx-4">
-                  <span class="fa fa-instagram"></span>
-                </a>
-                <a href="https://www.linkedin.com/in/muhammad-rafi-kusdiarto-265a09218/" class="btn btn-social-icon btn-linkedin me-4">
-                  <span class="fa fa-linkedin"></span>
-                </a>
-                <a href="https://www.facebook.com/profile.php?id=100005698544779" class="btn btn-social-icon btn-facebook">
-                  <span class="fa fa-facebook"></span>
-                </a>
-              </div>
-              <div class="flex">
-                <p>I am a frontend web developer with more than 2 months experience. I use Frontend libraries like ReactJS.</p>
-                <div class="my-5">
-                  <a href='https://api.whatsapp.com/send/?phone=6287809680106&text&app_absent=0' class="btn btn-success btn-lg me-3">Contact Me</a>
-                  <a href='./CV Muhammad Rafi Kusdiarto.pdf' class="btn btn-outline-success btn-lg" download>Download CV</a>
+    <section id="about">
+      <div class="hero">
+        <div class="container fluid">
+          <div class="row">
+            <div class="row d-flex justify-content-between my-5">
+              <img src="../assets/img/me.jpg" class="image col-xl-5 d-flex justify-content-center roundedCircle" alt="" />
+              <div class="col-6 mt-5 ">
+                <h2 class="my-2">Hello I'm</h2>
+                <h1><span>Muhammad Rafi Kusdiarto</span></h1>
+                <h5 class="mt-2">Frontend Web Developer</h5>
+                <div class="icon my-3">
+                  <a href="https://github.com/rafikusdiarto?tab=repositories" class="btn btn-social-icon btn-github">
+                    <span class="fa fa-github"></span>
+                  </a>
+                  <a href="https://www.instagram.com/muhmmad.rafii/" class="btn btn-social-icon btn-instagram mx-4">
+                    <span class="fa fa-instagram"></span>
+                  </a>
+                  <a href="https://www.linkedin.com/in/muhammad-rafi-kusdiarto-265a09218/" class="btn btn-social-icon btn-linkedin me-4">
+                    <span class="fa fa-linkedin"></span>
+                  </a>
+                  <a href="https://www.facebook.com/profile.php?id=100005698544779" class="btn btn-social-icon btn-facebook">
+                    <span class="fa fa-facebook"></span>
+                  </a>
                 </div>
-              </div>  
+                <div class="flex">
+                  <p>I am a frontend web developer with more than 2 months experience. I use Frontend libraries like ReactJS.</p>
+                  <div class="my-5">
+                    <a href='https://api.whatsapp.com/send/?phone=6287809680106&text&app_absent=0' class="btn btn-success btn-lg me-3">Contact Me</a>
+                    <a href='./CV Muhammad Rafi Kusdiarto.pdf' class="btn btn-outline-success btn-lg" download>Download CV</a>
+                  </div>
+                </div>  
+              </div>
             </div>
           </div>
         </div>
       </div>
+    </section>
     
     </>
   )
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,26 +1,43 @@
 import React from 'react'
 import './navbar.css'
 import {Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
 
-const nav = () => {
+const Nav = () => {
+const [navbar,setNavbar] = useState(false);
+
+const changeBackground=() =>{
+  if(window.scrollY > 0){
+    setNavbar(true)
+  }
+  else{
+    setNavbar(false)
+  }
+};
+
+useEffect(() => {
+  changeBackground()
+  window.addEventListener("scroll", changeBackground)
+})
+
   return (
     <>
-      <nav className="navbar sticky-top navbar-expand-lg shadow-lg mb-5">
+      <nav class={navbar ? "navbar active sticky-top navbar-expand-lg" : 'navbar'}>
         <div class="container-fluid">
           <a class="navbar-brand" href="#"><b>Rafi.</b></a>
           <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-            <ul class="navbar-nav">
+          <ul class="navbar-nav">
+            <li>
               <Link to="/" style={{textDecoration: 'none'}}>Home</Link>
               <Link to="/portfolioDetail" style={{textDecoration: 'none'}}>Portfolio</Link>
-            </ul>
-          </div>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
   )
 }
 
-export default nav
+export default Nav
